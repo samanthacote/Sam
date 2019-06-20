4e94f9d7cb17591b83fa629f4556b4a1e980c4f8
Samantha Cote <samanthacote@libp45p-18977d2.lm.lmig.com>
2019-06-20 10:54:05 -0400

    Added a gitignore

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,3 @@
 import React, {Component} from 'react';
 
-export class Dropdown extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isExpanded: false,
-        }
-    }
-
-    handleClick() {
-        this.setState({
-            isExpanded: !this.state.isExpanded,
-        })
-    }
-
-    render() {
-        if(this.state.isExpanded){
-        }
-    }
-}
+//TODO
